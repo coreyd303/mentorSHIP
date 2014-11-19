ba7b6d097d567f3090c6b004cefb3415fa50bfe1
Emily Davis <emilyadavis303@gmail.com>
2014-11-19 10:24:27 -0700

    Student can create profile - student#new & student#create tested.

--- spec/controllers/students_controller_spec.rb
+++ spec/controllers/students_controller_spec.rb
@@ -12,4 +12,13 @@ RSpec.describe StudentsController, type: :controller do
       expect(response).to render_template('new')
     end
   end
+
+  describe "create" do
+    describe "with valid params" do
+      it "saves the student in the database" do
+        @attributes = {name: "Harry Potter", bio: "Words about Harry Potter.", cohort: "1406", module_id: 1}
+        expect { post :create, student: @attributes}.to change(Student, :count).by 1
+      end
+    end
+  end
 end
