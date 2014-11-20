26e7e76895751aee7669e8c507ef2916bd85aae6
Corey Davis <coreyd303@gmail.com>
2014-11-20 13:52:11 -0700

    WIP

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,16 @@
 class StaticPagesController < ApplicationController
+before_action :get_mentors, only: [:index]
+before_action :get_student, only: [:index]
+
   def index
+  end
+
+private
+  def get_mentors
     @mentors = Mentor.all.sample(3)
+  end
+
+  def get_student
+    @student = Student.all.sample(1)
   end
 end
