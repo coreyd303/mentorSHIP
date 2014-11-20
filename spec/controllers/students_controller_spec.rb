53fa85b0bd24ce5e85ec994865c29fd55bc9831c
Emily Davis <emilyadavis303@gmail.com>
2014-11-20 10:32:04 -0700

    OCD indentation fixing in StudentsControllerSpec

--- spec/controllers/students_controller_spec.rb
+++ spec/controllers/students_controller_spec.rb
@@ -21,19 +21,29 @@ RSpec.describe StudentsController, type: :controller do
   describe "create" do
     describe "with valid params" do
       it "saves the student in the database" do
-        @attributes = { name: "Harry Potter", bio: "Words about Harry Potter.", cohort: "1406", module_id: 1 }
+        @attributes = { name: "Harry Potter",
+                        bio: "Words about Harry Potter.",
+                        cohort: "1406",
+                        module_id: 1 }
 
         expect { post :create, student: @attributes}.to change(Student, :count).by 1
       end
 
       it "saves the student in the database wihtout a bio" do
-        @attributes = { name: "Harry Potter", bio: nil, cohort: "1406", module_id: 1 }
+        @attributes = { name: "Harry Potter",
+                        bio: nil,
+                        cohort: "1406",
+                        module_id: 1 }
 
-        expect { post :create, student: @attributes}.to change(Student, :count).by 1
+        expect { post :create,
+                 student: @attributes}.to change(Student, :count).by 1
       end
 
       it "redirects to the new student's profile" do
-        @attributes = { name: "Harry Potter", bio: "Words about Harry Potter.", cohort: "1406", module_id: 1 }
+        @attributes = { name: "Harry Potter",
+                        bio: "Words about Harry Potter.",
+                        cohort: "1406",
+                        module_id: 1 }
         post :create, student: @attributes
 
         expect(response).to redirect_to Student.last
@@ -42,25 +52,40 @@ RSpec.describe StudentsController, type: :controller do
 
     describe "with invalid params" do
       it "does not save the new student without a name" do
-        @invalid_attributes = { name: nil, bio: "Words about Harry Potter.", cohort: "1406", module_id: 1 }
+        @invalid_attributes = { name: nil,
+                                bio: "Words about Harry Potter.",
+                                cohort: "1406",
+                                module_id: 1 }
 
-        expect { post :create, student: @invalid_attributes}.not_to change(Student, :count)
+        expect { post :create,
+                 student: @invalid_attributes}.not_to change(Student, :count)
       end
 
       it "does not save the new student without a cohort" do
-        @invalid_attributes = { name: "Harry Potter", bio: "Words about Harry Potter.", cohort: nil, module_id: 1 }
+        @invalid_attributes = { name: "Harry Potter",
+                                bio: "Words about Harry Potter.",
+                                cohort: nil,
+                                module_id: 1 }
 
-        expect { post :create, student: @invalid_attributes}.not_to change(Student, :count)
+        expect { post :create,
+                 student: @invalid_attributes}.not_to change(Student, :count)
       end
 
       it "does not save the new student without a module" do
-        @invalid_attributes = { name: "Harry Potter", bio: "Words about Harry Potter.", cohort: "1406", module_id: nil }
+        @invalid_attributes = { name: "Harry Potter",
+                                bio: "Words about Harry Potter.",
+                                cohort: "1406",
+                                module_id: nil }
 
-        expect { post :create, student: @invalid_attributes}.not_to change(Student, :count)
+        expect { post :create,
+                 student: @invalid_attributes}.not_to change(Student, :count)
       end
 
       it "re-renders the new template if student is not saved" do
-        @invalid_attributes = { name: "Harry Potter", bio: "Words about Harry Potter.", module_id: 1 }
+        @invalid_attributes = { name: "Harry Potter",
+                                bio: "Words about Harry Potter.",
+                                cohort: nil, 
+                                module_id: 1 }
         post :create, student: @invalid_attributes
 
         expect(response).to render_template('new')
@@ -72,7 +97,7 @@ RSpec.describe StudentsController, type: :controller do
     it "assigns the requested student to @student" do
       user = student
       get :show, id: student
-      
+
       expect(user).to eq(student)
     end
 
