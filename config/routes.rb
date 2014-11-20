82758c22bd22446f317819cf617d67fa45f7f366
Chad Brading <chadbrading@gmx.com>
2014-11-20 10:26:48 -0700

    fixes sessions destroy route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   get '/auth/:provider/callback', to: 'sessions#create'
   get 'profile_type_prompt', to: 'static_pages#profile_type_prompt'
+  get '/sign_out', to: 'sessions#destroy'
 
   resources :mentors
   resources :skills
