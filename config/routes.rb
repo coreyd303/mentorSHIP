27b68e24c41afc9112511abae1423853e77d3839
Corey Davis <coreyd303@gmail.com>
2014-11-24 14:23:19 -0700

    fixed duplicates in the navbar

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
 
   root to: 'static_pages#index'
 
-  get '/home',                    to: 'static_pages#home'
   get '/auth/:provider/callback', to: 'sessions#create'
-  get 'signout',                  to: 'sessions#destroy', as: 'signout'
+  get '/signout',                 to: 'sessions#destroy', as: 'signout'
   get '/search',                  to: 'mentors#search'
   get 'profile_type_prompt',      to: 'static_pages#profile_type_prompt'
 
