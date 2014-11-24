1ececb01300312014ff3f92a02554f401f776341
Corey Davis <coreyd303@gmail.com>
2014-11-24 14:09:32 -0700

    fixing issue when there are no mentors in the db

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,20 +1,18 @@
 class StaticPagesController < ApplicationController
-# before_action :get_mentors, only: [:index]
-# before_action :get_student, only: [:index]
+before_action :get_mentors, only: [:index]
+before_action :get_student, only: [:index]
 
   def index
-    @mentors = Mentor.all.sample(3)
-    @student = Student.all.sample(3).pop
   end
 
 private
 
   def get_mentors
-
+    @mentors = Mentor.all.sample(3)
   end
 
   def get_student
-    
+    @student = Student.all.sample(3).pop
   end
 
   def profile_type_prompt
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   root to: 'static_pages#index'
 
+  get '/home',                    to: 'static_pages#home'
   get '/auth/:provider/callback', to: 'sessions#create'
   get 'signout',                  to: 'sessions#destroy', as: 'signout'
   get '/search',                  to: 'mentors#search'
