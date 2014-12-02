111c4cba99a0fc7527b246d2d61ab3ff4bb4c0fc
timproctor <timandcamiproctor@gmail.com>
2014-12-01 20:38:56 -0700

    added key/value based cache to mentor/index

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,7 +13,7 @@ Rails.application.configure do
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
-  config.cache_store = dalli_store
+  config.cache_store = :dalli_store
   config.action_controller.perform_caching = true
 
   # Don't care if the mailer can't send.
