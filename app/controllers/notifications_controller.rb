f075de7334cd3e9cd32f3281b9eef2b4fba8d818
Corey Davis <coreyd303@gmail.com>
2014-12-02 09:57:34 -0700

    WIP

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,15 +2,16 @@ class NotificationsController < ApplicationController
  
   skip_before_action :verify_authenticity_token
  
-  def notify
+  def notify(number, body)
     sid   = Figaro.env.twilio_account_sid
     token = Figaro.env.twilio_auth_token
+    twilio_number = "303-816-3192"
 
     client = Twilio::REST::Client.new(sid, token)
 
-    client.account.sms.messages.create(from: '+1 303-816-3192', 
-                                       to: '+1 303-249-6458', 
-                                       body: 'Learning to send SMS you are.')
+    client.account.sms.messages.create(from: '+1 #{twilio_number}', 
+                                       to: number, 
+                                       body: body)
 
   end
  
