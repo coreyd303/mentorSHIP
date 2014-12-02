24922a489e7d5129179b51ecd5a0dd15e91d4c97
Corey Davis <coreyd303@gmail.com>
2014-12-02 13:19:34 -0700

    taking out unneeded methods

--- app/models/mentor.rb
+++ app/models/mentor.rb
@@ -8,39 +8,4 @@ class Mentor < ActiveRecord::Base
 
   has_attached_file :photo, :styles => { medium: "300x300>", thumb: "150x150"},
   :default_url => "turing.png"
-
-  # def self.find_matches(query)
-  #   query = query.downcase
-  #   where("lower(name) like ?", "%#{query}%")
-  # end
-
-  # def self.contact_by
-  #   ["Text-Request Appointment","Email-Request Appointment"]
-  # end
-
-  # def self.send_text_message
-  #   twilio_sid   = ENV["twilio_account_sid"]
-  #   twilio_token = ENV["twilio_account_token"]
-
-  #   @twilio_client = Twilio::REST::Client.new(twilio_sid, twilio_token)
-
-  #   @twilio_client.account.sms.messages.create(
-  #     :from => "+16572206498",
-  #     :to   => "+17144012809",
-  #     :body => "Hey there"
-  #   )
-  # end
-
-  # def skills(skill_list)
-  #   if skill_list
-  #     valid_skills = skill_list.reject do |skill|
-  #      skill.empty?
-  #     end
-  #     new_or_found_skills = valid_skills.map do |name|
-  #       Skill.find_or_create_by(name: name)
-  #     end
-
-  #     self.skills = new_or_found_skills
-  #   end
-  # end
 end
