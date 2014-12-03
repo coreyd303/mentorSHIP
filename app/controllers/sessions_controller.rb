5ec430d616712dfb97753f12a5d7a199a8cbf0d8
Corey Davis <coreyd303@gmail.com>
2014-12-03 16:33:20 -0700

    WIP

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,25 @@
 class SessionsController < ApplicationController
   def create
     auth  = request.env["omniauth.auth"]
-    @user = User.find_by_omniauth(auth)
+    # @user = User.find_by_omniauth(auth)
+    
+    @user = User.from_omniauth(env["omniauth.auth"])
     if @user
-      session[:user_id] = @user.id
+      session[:user_id] = user.id
+      @user
+      redirect_to root_url
     else
-      @user             = User.create_with_omniauth(auth)
+      @user             = User.create_with_auth(auth)
       session[:user_id] = @user.id
       redirect_to profile_type_prompt_path
     end
+    # if @user
+    #   session[:user_id] = @user.id
+    # else
+    #   @user             = User.create_with_omniauth(auth)
+    #   session[:user_id] = @user.id
+    #   redirect_to profile_type_prompt_path
+    # end
   end
 
   def destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,13 +11,33 @@ class User < ActiveRecord::Base
     Mentor.find(profile_id) if profile_type == 'mentors'
   end
 
-  def self.find_by_omniauth(auth)
-    user = User.find_by_uid(auth['uid'])
+  def self.from_omniauth(auth)
+    user = find_with_auth(auth)
+    create_with_auth(auth) unless user
+    user
   end
 
-  def self.create_with_omniauth(auth)
-    create! do |user|
-      user.uid = auth['uid']
+  def self.find_with_auth(auth)
+    User.find_by(
+      uid: auth.uid
+      )
+  end
+
+  def self.create_with_auth(auth)
+    User.new do |user|
+      user.uid = auth.uid
+      user.save!
     end
   end
+
+  # def self.find_by_omniauth(auth)
+  #   user = User.find_by_uid(auth['uid'])
+  # end
+
+  # def self.create_with_omniauth(auth)
+  #   create! do |user|
+  #     user.uid = auth['uid']
+  #   end
+  # end
 end
+
