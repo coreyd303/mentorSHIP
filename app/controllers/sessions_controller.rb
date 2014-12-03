dbea6254410a324196c179352c5633425c576a43
Corey Davis <coreyd303@gmail.com>
2014-12-03 16:44:31 -0700

    fixes issue on user create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,10 @@
 class SessionsController < ApplicationController
   def create
-    # auth  = request.env["omniauth.auth"]
-    # @user = User.find_by_omniauth(auth)
-    
-    user = User.from_omniauth(env["omniauth.auth"])
+    auth = request.env["omniauth.auth"]
+    user = User.from_omniauth(auth) || User.create_with_auth(auth)
     session[:user_id] = user.id
     user
     redirect_to root_url
-
-    # if @user
-    #   session[:user_id] = @user.id
-    # else
-    #   @user             = User.create_with_omniauth(auth)
-    #   session[:user_id] = @user.id
-    #   redirect_to profile_type_prompt_path
-    # end
   end
 
   def destroy
