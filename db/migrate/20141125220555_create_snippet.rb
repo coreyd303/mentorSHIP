542c765a173d78cc4997074967248de7b634d426
coreyd303 <coreythedev@gmail.com>
2014-12-03 11:11:38 -0700

    Update 20141125220555_create_snippet.rb

--- db/migrate/20141125220555_create_snippet.rb
+++ db/migrate/20141125220555_create_snippet.rb
@@ -2,8 +2,8 @@ class CreateSnippet < ActiveRecord::Migration
   def change
     create_table :snippets do |t|
       t.integer :student_id
-      t.text :title
-      t.text :body
+      t.text    :title
+      t.text    :body
     end
   end
 end
